5dd916f3cf58a75cd3e365a5fbf73d34ef0d1b41
vikas-saini-7 <vikas289888@gmail.com>
2024-06-03 12:48:38 +0530

    bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,14 +1,28 @@
+import java.util.Arrays;
+
 public class BubbleSort {
     public static void main(String[] args) {
-        
+        int arr[] = {1, 3, 4, 5};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void bubbleSort(int[] arr) {
+    static void bubble(int[] arr) {
+        boolean swapped = false;
         for(int i = 0; i< arr.length; i++){
-            for(int j; j < arr.length - i; j++){
-            if(arr[j] < arr[j-1])
-                // swap 
-                
+            swapped = false;
+            for(int j = 1; j < arr.length - i; j++){
+                if(arr[j] < arr[j-1]){
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            System.out.println(swapped);
+            if(!swapped){
+                break;
             }
         }
     }

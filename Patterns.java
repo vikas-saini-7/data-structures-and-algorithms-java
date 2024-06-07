2aa75ea0451effc92c2382e59658004400e056a4
vikas-saini-7 <vikas289888@gmail.com>
2024-06-07 12:02:31 +0530

    patterns

--- Patterns.java
+++ Patterns.java
@@ -1,10 +1,109 @@
 public class Patterns {
     public static void main(String[] args) {
-        
+        pattern6(5);
     }
 
+    // * * * *
+    // * * * *
+    // * * * *
+    // * * * *
     static void pattern1(int n){
-        
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+
+    // *
+    // * *
+    // * * *
+    // * * * *
+    static void pattern2(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+    
+    // * * * * *
+    // * * * *
+    // * * *
+    // * *
+    // *
+    static void pattern3(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = n; col >= row; col--) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+
+    // 1
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+    // 1 2 3 4 5
+    static void pattern4(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col+" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    static void pattern5(int n){
+
+    }
+
+    //      *
+    //     **
+    //    ***
+    //   ****
+    //  *****
+    static void pattern6(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                if(col==row){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    //    *****
+    //     ****
+    //      ***
+    //       **
+    //        *
+    static void pattern7(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                if(row > col){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
     }
 
 }
